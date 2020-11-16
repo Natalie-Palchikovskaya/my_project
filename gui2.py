bef0b453f454e3dc84ab6e0703a10fee92748173
Natalie <you@example.com>
2020-11-16 13:39:43 +0300

    Add 1st version of GUI

---
+++ gui2.py
@@ -0,0 +1,75 @@
+import sys
+from PyQt5.QtWidgets import (QWidget, QPushButton, QLineEdit,
+    QInputDialog, QApplication, QLabel, QHBoxLayout)
+from PyQt5.QtGui import QColor
+from PyQt5.QtGui import QPixmap
+from neuroflowers import predict
+
+class main_gui(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+        self.btn = QPushButton('Download picture', self)
+        self.btn.move(20, 20)
+        self.btn.clicked.connect(self.showDialog)
+
+        self.le = QLineEdit(self)
+        self.le.move(130, 22)
+
+        self.btn1 = QPushButton('What is the flower?', self)
+        self.btn1.move(20, 48)
+        self.btn1.clicked.connect(self.flower)
+        self.le1 = QLineEdit(self)
+        self.le1.move(130, 50)
+
+        self.lab = QLabel(self)  # для вывода нередактируемого текста
+        self.lab.setText("Accuracy, %:")
+        self.lab.move(130, 70)
+
+        self.setGeometry(1000, 500, 750, 400)
+        self.setWindowTitle('Guess the flower')
+        self.show()
+
+    def flower(self):
+        self.le1.setText("Processing...")
+        label, percent = predict(str(path))
+        self.le1.setText(label)
+
+
+        self.lab1 = QLabel(self)  # для вывода нередактируемого текста
+        self.lab1.setText(str(percent))
+        self.lab1.move(200, 70)
+        self.lab1.show()
+
+
+
+    def showDialog(self):
+        img_path, ok = QInputDialog.getText(self, 'Input',
+            'Enter path to flower:')
+        global path
+        path = img_path
+
+        if ok:
+            self.le.setText(str(path))
+            pixmap = QPixmap(str(path))
+            pixmap = pixmap.scaled(256,256)
+            self.lbl = QLabel(self)
+            self.lbl.setPixmap(pixmap)
+            self.lbl.move(350, 20)
+            self.lbl.show()
+            print(path)
+
+
+
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    gui = main_gui()
+    sys.exit(app.exec_())
